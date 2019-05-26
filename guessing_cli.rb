40b225bb2c343629f25e811dbcbb711e5fa77d9d
Jason Leach <jlspursfan@gmail.com>
2019-05-26 21:01:18 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     number = (rand(6))
     input = gets.chomp
 
-    if input == number
+    if input.to_i == number
       puts "You guessed the correct number!"
     elsif input == "exit"
       puts "Goodbye!"
