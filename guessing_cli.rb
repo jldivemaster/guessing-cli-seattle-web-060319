89f2b602abfd996452b805444662f41b456bd0a0
Jason Leach <jlspursfan@gmail.com>
2019-05-26 20:29:44 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,14 @@
 # Code your solution here!
+def run_guessing_game
+  number = (rand)
+  input = gets.chomp
+
+  if input == "exit"
+    break
+  elsif input == number
+    puts "You guessed the correct number!"
+  else
+    puts "The computer guessed #{number}."
+  end
+  
+end
